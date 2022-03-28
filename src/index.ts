cd80bbb38649a3f648d1786a74d6166c93d3c30a
CH MD <39788804+medchek@users.noreply.github.com>
2022-03-28 08:16:51 +0100

    make entry file as export only

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,2 @@
-// import { doesGridContainZero } from "./utils/utils";
-
-import { Sudoku } from "./core/Sudoku";
-import { validateSudokuGrid } from "./utils/validator";
-
-console.clear();
-const sudoku = new Sudoku({ debug: false });
-console.log("Starting...");
-
-const perfStart = performance.now();
-sudoku.generate();
-
-// for (let i = 0; i < 1000; i++) {
-//   sudoku.generate();
-// }
-
-const perfEnd = performance.now();
-const totalTime = (perfEnd - perfStart).toFixed(5);
-console.log(`Time taken: ${totalTime}ms`);
-console.table(sudoku.grid);
-console.log("IS A VALID SUDOKU GRID? => ", validateSudokuGrid(sudoku.grid));
-console.log(sudoku.grid);
+// import { Sudoku } from "./core/Sudoku";
+export { Sudoku } from "./core/Sudoku";
